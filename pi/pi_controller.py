4999edb61dffde21286f73e70a352482a1596975
Ahmad Ghaemi <ah2177gh-s@student.lu.se>
2021-11-23 15:06:16 +0100

    Hej! Det här är nya filen där du har lagt till en funktion som styr drönaren med en joystick. Kolla i pi_controller.py. :-) MVH /Du

--- pi/pi_controller.py
+++ pi/pi_controller.py
@@ -2,13 +2,53 @@ import requests
 import time
 import random
 import click
+from sense_hat import SenseHat
+sense = SenseHat()
 
-
+        
 def get_direction():
     d_long = 0
     d_la = 0
     send_vel = False
-    c = click.getchar()
+    
+    dir = ""
+    for event in sense.stick.get_events():
+        dir = event.direction
+    if dir != '':
+        if dir == 'left':
+                click.echo('Left')
+                send_vel = True
+                d_long = -1
+                d_la = 0
+        elif dir == 'right':
+                click.echo('Right')
+                send_vel = True
+                d_long = 1
+                d_la = 0
+        elif dir == 'up':
+                click.echo('Up')
+                send_vel = True
+                d_long = 0
+                d_la = 1
+        elif dir == 'down':
+                click.echo('Down')
+                send_vel = True
+                d_long = 0
+                d_la = -1
+        else:
+                d_long = 0
+                d_la = 0
+                click.echo('Invalid input :(')
+                send_vel = False
+    return d_long, d_la, send_vel
+
+'''
+def get_direction():
+    d_long = 0
+    d_la = 0
+    send_vel = False
+
+c = click.getchar()
     if c =='a':
         click.echo('Left')
         send_vel = True
@@ -35,7 +75,7 @@ def get_direction():
         click.echo('Invalid input :(')
         send_vel = False
     return d_long, d_la, send_vel
-
+'''
 
 if __name__ == "__main__":
     SERVER_URL = "http://127.0.0.1:5000/drone"
